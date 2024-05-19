0a8356dfcffbd69b9625f909eb09b2f403aed3ff
SGrottel <sgrottel@sgrottel.de>
2024-05-19 12:09:16 +0200

    Warn entries without online references

--- Model/ListFile.cs
+++ Model/ListFile.cs
@@ -31,6 +31,34 @@ namespace MoviesList.Model
 				if (!list.Type.Equals(yamlFileType)) throw new Exception($"Yaml file must be of type '{yamlFileType}'");
 				if (new Version(list.Version) != new Version(1, 2)) throw new Exception("Yaml file must be of version '1.2'");
 
+				foreach (Entry e in list.Collection)
+				{
+					if (e.Homebrewn ?? false) continue;
+
+					if (string.IsNullOrEmpty(e.Imdb) && string.IsNullOrEmpty(e.AniDB))
+					{
+						bool allInnerEntriesOnline = false;
+						if ((e.Box?.Length ?? 0) > 0)
+						{
+							allInnerEntriesOnline = true;
+							foreach (Entry e2 in e.Box!)
+							{
+								if (string.IsNullOrEmpty(e2.Imdb) && string.IsNullOrEmpty(e2.AniDB))
+								{
+									allInnerEntriesOnline = false;
+									break;
+								}
+							}
+						}
+
+						if (!allInnerEntriesOnline)
+						{
+							// entry without online db reference!
+							Console.WriteLine($"WARNING: no online reference on {e.Title}");
+						}
+					}
+				}
+
 				return list;
 			}
 		}
